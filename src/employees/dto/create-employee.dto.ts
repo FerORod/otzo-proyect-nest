16cc08e3b3a8824331fba4e24c80537020fc2ebb
FerORod <fer.o.rodriguez.08@gmail.com>
2026-09-21 20:28:06 -0600

    CRUD employees updated

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,6 +1,16 @@
+import { IsString, IsUUID, MaxLength, maxLength } from "class-validator";
+
 export class CreateEmployeeDto {
+    @IsUUID()
+    @IsString()
     id?: string;
+    @IsString()
+    @MaxLength(30)
     name?: string;
+    @IsString()
+    @MaxLength(30)
     lastName?: string;
+    @IsString()
+    @MaxLength(12)
     phoneNumber?: string;
 }
